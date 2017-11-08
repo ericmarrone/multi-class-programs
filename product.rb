ac95c134a8300e7a35c669dfe66a15120d735a06
Eric Marrone <emarrone17@gmail.com>
2017-11-08 14:05:09 -0500

    Add shelf to product file

--- product.rb
+++ product.rb
@@ -1,8 +1,11 @@
 class Product
-  def initialize(name, price, tax_rate)
+  @@shelf = []
+  def initialize(name, base_price, tax_rate)
     @name = name
-    @base_price = price
+    @base_price = base_price
     @tax_rate = tax_rate
+
+    @@shelf << self
 
   end
 
@@ -20,15 +23,34 @@ class Product
     @tax_rate
   end
 
+  # writers
+  def name=(name)
+    @name = name
+  end
+
+  def base_price=(base_price)
+    @base_price = base_price
+  end
+
+  def tax_rate=(tax_rate)
+    @tax_rate = tax_rate
+  end
+
   #instance methods
+
 
   def total_price
     self.base_price * (1 + self.tax_rate)
   end
 
+  def self.shelf
+    @@shelf
+  end
 
 end
 
 tb = Product.new("TB", 2.0, 0.15)
-puts tb.inspect
-puts tb.total_price
+tg = Product.new("Tg", 2.0, 0.15)
+# puts tb.inspect
+# puts tb.total_price
+puts Product.shelf.inspect
